3c4e12f60e813d6b8db01a3165e08437546b2e24
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-04 11:11:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
-}
+
+const input = document.getElementById('input');
+
+
+  function addingEventListener(i){
+    input.addEventListener('click', function(event) {
+        alert('I was clicked!');
+      })
+    }
+
+  addEventListener(input);
